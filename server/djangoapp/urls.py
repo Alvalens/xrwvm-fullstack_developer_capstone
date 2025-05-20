7be52db9feb93c5ae36300385a5adb04d0546a50
alvalens <alvalen.shafel04@gmail.com>
2025-05-20 10:32:43 -0400

    complete module 3.2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+
+    # cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
     
     # path for dealer reviews view
 
